fb7e0f83eea87e7d4dbb4628fa27f29dbd861e78
Diego Aguilar and Saul Artiga <diego+saul@>
2017-06-08 16:39:31 -0700

    work on backend js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
+function cryptosquare(str){
+
+  str = str.replace(/\s+/g, '');
+  //variables to be used
+  var strArray = str.split("");
+  var charTotal = strArray.length;
+  var columns = Math.floor(Math.sqrt(charTotal));
+  var rows = Math.ceil(Math.sqrt(charTotal));
+
+  // if (charTotal % columns === 0){
+  //   rows = columns;
+  // }
+
+  $("body").append("<H5>Character Count: "+charTotal+"</H5>");
+  $("body").append("<H5>Columns: "+columns+"</H5>");
+  $("body").append("<H5>Rows: "+rows+"</H5>");
+}
+
 $(document).ready(function() {
-  
+  cryptosquare("bearsaa");
 });
