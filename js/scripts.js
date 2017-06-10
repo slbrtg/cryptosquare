1853da548f7f580b24a3a51dfeb166c62949cb77
Saul Artiga <slbrtg@gmail.com>
2017-06-10 16:24:45 -0700

    create functional js code to create a cryptosquare

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 function cryptosquare(str){
 
   str = str.replace(/\s+/g, '');
-  //variables to be used
+  //variables to be used in gathering column and row length
   var strArray = str.split("");
   var charTotal = strArray.length;
   var columns = Math.floor(Math.sqrt(charTotal));
   var rows = Math.floor(Math.sqrt(charTotal));
-
+  //variables to be used when creating the output
+  var outputArray;
+  var outputStr;
+  var chunk = columns;
   //assigns rows value based on length of inputted string
   if (charTotal - (columns*columns) > columns){
     rows += 2;
@@ -15,9 +18,13 @@ function cryptosquare(str){
   } else {
     rows = rows;
   }
-
-
-
+  //creates new arrays based on column, row, and element length
+  //outputs results to html
+  for (i = 0; i < charTotal; i+=chunk) {
+    outputArray = strArray.slice(i,i+chunk);
+    outputStr = outputArray.join('|');
+    $("body").append("<H5>|"+outputStr+"|</H5>");
+  }
 
 
 
@@ -27,5 +34,5 @@ function cryptosquare(str){
 }
 
 $(document).ready(function() {
-  cryptosquare("11");
+  cryptosquare("hello there im saul");
 });
